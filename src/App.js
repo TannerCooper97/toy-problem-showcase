7433810d9fe97aff323c578f7799e050b1b23c4e
TannerCooper97 <tannerccooper@gmail.com>
2021-03-02 17:24:10 -0700

    Completed Lab

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import './index.css';
+import TopicBroswer from './components/TopicBrowser/TopicBrowser'
+
 
 class App extends Component {
   render() {
-    
+    return(
+      <TopicBroswer />
+      
+      )
   }
 }
 
